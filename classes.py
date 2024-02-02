4b7dc0cfa167742b29afa824b8707f0a59e27650
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-02 22:08:48 +0200

    add reset_deck to Deck

--- classes.py
+++ classes.py
@@ -12,6 +12,9 @@ class Card:
 
 class Deck:
 	def __init__(self):
+		self.reset_deck()
+
+	def reset_deck(self):
 		suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 		ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 		self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
