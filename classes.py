6a9d0b9cf9c8fee3add18c8aa6712e0d0b0b0abb
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-03 00:39:02 +0200

    add check_hand to Hand, rename 10 to T

--- classes.py
+++ classes.py
@@ -16,7 +16,7 @@ class Deck:
 
 	def reset_deck(self):
 		suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-		ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+		ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 		self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
 		self.shuffle()
 
@@ -30,24 +30,68 @@ class Deck:
 
 
 class Hand(Card):
-
 	def __init__(self, cards:list):
 		super().__init__('', '')
-		self.reset_cards(cards)
 
-	def reset_cards(self, cards:list):
-		self.ranks = {rank: 0 for rank in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']}
+	def count_cards(self):
+		self.ranks = {rank: 0 for rank in ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']}
 		self.suits = {suit: 0 for suit in ['Hearts', 'Diamonds', 'Clubs', 'Spades']}
 
 		for card in self.cards:
 			self.ranks[card.rank] += 1
 			self.suits[card.suit] += 1
 
-	def check_hand(self):
-		pass
+	@classmethod
+	def check_hand(self, cards:list):
+		self.cards = cards
+		self.count_cards(self)
+		if self.check_royal_flush(self):
+			return f"You got a royal flush!"
+		elif self.check_straight_flush(self):
+			return f"You got a straight flush!"
+		elif self.check_same_of_a_kind(self, 4):
+			return f"You got four-of-a-kind!"
+		elif self.check_same_of_a_kind(self, 3) and self.check_same_of_a_kind(self, 2):
+			return f"You got a full house!"
+		elif self.check_flush(self):
+			return f"You got a flush!"
+		elif self.check_straight(self):
+			return f"You got a straight!"
+		elif self.check_same_of_a_kind(self, 3):
+			return f"You got three-of-a-kind!"
+		elif self.check_pairs(self, 2):
+			return f"You got 2 pairs!"
+		elif self.check_pairs(self, 1):
+			return f"You got a pair!"
+		else:
+			return f"You got {self.cards[4].rank} high!"
+
+	def check_royal_flush(self):
+		if self.check_flush(self) and self.check_straight(self) and self.cards[4].rank == 'A':
+			return True
+		return False
+
+	def check_straight_flush(self):
+		if self.check_flush(self) and self.check_straight(self):
+			return True
+		return False
 
 	def check_straight(self):
-		pass
+		ranks_high = '23456789TJQKA'
+		rank_indeces_high = [ranks_high.index(card.rank) for card in self.cards]
+		ranks_low = 'A23456789TJQK'
+		rank_indeces_low = [ranks_low.index(card.rank) for card in self.cards]
+
+		if max(rank_indeces_high) - min(rank_indeces_high) == 4 and len(set(rank_indeces_high)) == 5:
+			return True
+		elif max(rank_indeces_low) - min(rank_indeces_low) == 4 and len(set(rank_indeces_low)) == 5:
+			return True
+		else:
+			return False
+
+	def check_pairs(self, amount:int):
+		pairs = [rank for rank in self.ranks.values() if rank == 2]
+		return len(pairs) == amount
 
 	def check_flush(self):
 		if 5 in self.suits.values():
@@ -60,19 +104,18 @@ class Hand(Card):
 		return False
 
 
-
-
 class Player:
 	def __init__(self):
 		self.hand = []
 
 	def check_hand(self):
 		self.sort_hand()
-		return Hand.check_hand()
+		print(self)
+		print(Hand.check_hand(self.hand))
 
 	def sort_hand(self):
 		def rank_value(card):
-			rank_order = {'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, '10': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12}
+			rank_order = {'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, 'T': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12}
 			return rank_order.get(card.rank, 0)
 		self.hand.sort(key=lambda card: (rank_value(card), card.suit))
 
@@ -100,5 +143,4 @@ class Game:
 	def start_game(self):
 		self.deck.reset_deck()
 		self.player.hand = self.deck.deal_cards(5)
-		print(self.player)
-		print(self.player.check_hand())
+		self.player.check_hand()
