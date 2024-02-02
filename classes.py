452a35426ae717ed3d2efe6c38675519c9f7f1c6
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-02 22:02:36 +0200

    add iteration and str to Player

--- classes.py
+++ classes.py
@@ -6,6 +6,9 @@ class Card:
 		self.suit = suit
 		self.rank = rank
 
+	def __str__(self):
+		return f"{self.rank} of {self.suit}"
+
 
 class Deck:
 	def __init__(self):
@@ -27,6 +30,22 @@ class Player:
 		self.hand = []
 
 
+	def __iter__(self):
+		self.n = 0
+		return self
+
+	def __next__(self):
+		if self.n < len(self.hand):
+			card = self.hand(self.n)
+			self.n += 1
+			return card
+		else:
+			raise StopIteration
+
+	def __str__(self):
+		return f"Hand:\n{', '.join(map(str, self.hand))}"
+
+
 class Game:
 	def __init__(self):
 		self.deck = Deck()
@@ -34,3 +53,4 @@ class Game:
 	def start_game(self, player:Player):
 		self.deck.shuffle()
 		player.hand = self.deck.deal_cards(5)
+		print(player)
