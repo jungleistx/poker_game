122e26ad78cfdf147b5e0d8a50b9ce02b800a9b3
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-14 14:06:06 +0200

    mod Hand, fix typo indentation error

--- classes/hand.py
+++ classes/hand.py
@@ -23,7 +23,7 @@ class Hand():
 		self.cards.extend(new_cards)
 
 	def sort_hand(self):
-		 def rank_value(card):
+		def rank_value(card):
 			rank_order = {'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, 'T': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12}
 			return rank_order.get(card.rank, 0)
 		self.cards.sort(key=lambda card: (rank_value(card), card.suit))
