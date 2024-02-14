69b45df60f2814036be1ac8262e0d7f8f4300d8e
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-14 18:29:43 +0200

    mod Image, add __str__ and rm print_coordinates()

--- classes/image.py
+++ classes/image.py
@@ -13,14 +13,11 @@ class Image():
 	def is_clicked(self, mouse_pos) -> bool:
 		return self.rect.collidepoint(mouse_pos)
 
-	def print_coordinates(self):
-		print(f'({self.x},{self.y})', end=' - ')
-		print(f'({self.x + self.width},{self.y})')
-		print(f'({self.x},{self.y + self.height})', end=' - ')
-		print(f'({self.x + self.width},{self.y + self.height})')
-
 	def set_current_rect(self):
 		self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
 	def draw_image(self, window):
 		window.blit(self.image, (self.x, self.y))
+
+	def __str__(self) -> str:
+		return f"\'{self.__image_path}\':\n({self.x},{self.y}) - ({self.x + self.width},{self.y + self.height})"
