11b89ab6f2e87eab1486f738a3db6d5d0b7d76d9
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-14 14:59:43 +0200

    mod Button, add pygame_instance import from main and use in init

--- classes/button.py
+++ classes/button.py
@@ -1,15 +1,16 @@
 from classes.image import Image
+from main import pygame_instance
 
 
 class Button:
 	BUTTON_HEIGHT = 80
 	BUTTON_WIDTH = 160
 
-	def __init__(self, text:str, pygame, x:int=0, y:int=0, b_x:int=0, b_y:int=0):
+	def __init__(self, text:str, x:int=0, y:int=0, b_x:int=0, b_y:int=0):
 		self.text = text
-		self.image = Image('img/button/button.png', pygame, x, y)
-		self.image.image = pygame.transform.scale(self.image.image, (Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT))
-		self.game_font = pygame.font.SysFont('chalkboard', 28)
+		self.image = Image('img/button/button.png', x, y)
+		self.image.image = pygame_instance.transform.scale(self.image.image, (Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT))
+		self.game_font = pygame_instance.font.SysFont('chalkboard', 28)
 		self.text_x = b_x
 		self.text_y = b_y
 
