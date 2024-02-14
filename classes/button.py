92d873abaf79213ee5cb6b232d680287f33d55b9
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-14 15:00:16 +0200

    mod Button, clean variable names

--- classes/button.py
+++ classes/button.py
@@ -3,13 +3,13 @@ from main import pygame_instance
 
 
 class Button:
-	BUTTON_HEIGHT = 80
-	BUTTON_WIDTH = 160
+	HEIGHT = 80
+	WIDTH = 160
 
 	def __init__(self, text:str, x:int=0, y:int=0, b_x:int=0, b_y:int=0):
 		self.text = text
 		self.image = Image('img/button/button.png', x, y)
-		self.image.image = pygame_instance.transform.scale(self.image.image, (Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT))
+		self.image.image = pygame_instance.transform.scale(self.image.image, (Button.WIDTH, Button.HEIGHT))
 		self.game_font = pygame_instance.font.SysFont('chalkboard', 28)
 		self.text_x = b_x
 		self.text_y = b_y
