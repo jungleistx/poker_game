70193c77df347afe135899772109400a6f7403ae
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-14 13:49:40 +0200

    mod window, add Game to self.game

--- classes/window.py
+++ classes/window.py
@@ -16,6 +16,7 @@ class Window:
 		self.window = pygame.display.set_mode((Window.width, Window.height))
 		self.__set_background()
 		self.__set_buttons()
+		self.game = Game()
 
 	def __set_background(self):
 		self.background = Image('img/background/poker_table.jpg')
@@ -27,9 +28,9 @@ class Window:
 		self.swap = Button('Swap cards', swap_button_location)
 		self.submit = Button('Continue', submit_button_location)
 
-	def check_mouseclick_cards(self, game:Game):
+	def check_mouseclick_cards(self):
 		mouse_pos = pygame.mouse.get_pos()
-		for card in game.player.hand:
+		for card in self.game.player.hand:
 			if card.image.is_clicked(mouse_pos) and card.swapping == False:
 				card.image.y -= 50
 				card.swapping = True
@@ -37,44 +38,44 @@ class Window:
 				card.swapping = False
 				card.image.y += 50
 
-	def check_mouseclick_buttons(self, game:Game):
+	def check_mouseclick_buttons(self):
 		mouse_pos = pygame.mouse.get_pos()
 		self.swap.update_coordinates()
 		if self.swap.image.is_clicked(mouse_pos):
-			if not game.swap_used:
-				game.player.check_swaps(game)
+			if not self.game.swap_used:
+				self.game.player.check_swaps(self.game)
 			else:
 				print('Swap used!')
 		elif self.submit.image.is_clicked(mouse_pos):
-			game.player.check_hand()
+			self.game.player.check_hand()
 
 	def draw_buttons(self):
 		self.swap.draw_button(self.window)
 		self.submit.draw_button(self.window)
 
-	def draw_cards(self, game:Game):
-		for card in game.player.hand:
+	def draw_cards(self):
+		for card in self.game.player.hand:
 			card.image.draw_image(self)
 
-	def set_card_locations(self, game:Game):
+	def set_card_locations(self):
 		card_width = 140
 		card_gap = 50
 		x = 100
-		for card in game.player.hand:
+		for card in self.game.player.hand:
 			card.image.x = x
 			card.image.set_current_rect()
 			x += card_gap + card_width
 
-	def check_event_keys(self, event, game:Game):
+	def check_event_keys(self, event):
 		if event.key == pygame.K_s:
-			if not game.swap_used:
-				game.player.check_swaps(game)
+			if not self.game.swap_used:
+				self.game.player.check_swaps(self.game)
 			else:
 				print('Swap used!')
 		if event.key == pygame.K_c:
-			game.player.check_hand()
+			self.game.player.check_hand()
 
-	def check_events(self, game:Game):
+	def check_events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				print('Thank you come again!')
@@ -82,10 +83,10 @@ class Window:
 				exit()
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if event.button == 1:
-					self.check_mouseclick_cards(game)
-					self.check_mouseclick_buttons(game)
+					self.check_mouseclick_cards()
+					self.check_mouseclick_buttons()
 			if event.type == pygame.KEYDOWN:
-				self.check_event_keys(event, game)
+				self.check_event_keys(event)
 
 	def run(self, game:Game):
 		# intro screen
