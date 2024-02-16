327ac6be61873b4aad725bfac6696b1bf6c513e6
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-02-16 11:15:33 +0200

    mod Button, add TEXT_COLOUR as class variable

--- classes/button.py
+++ classes/button.py
@@ -5,6 +5,8 @@ from pygame_manager import pygame_instance, game_font
 class Button:
 	HEIGHT = 80
 	WIDTH = 160
+	TEXT_COLOUR = (2, 36, 8)
+
 
 	def __init__(self, text:str, x:int=0, y:int=0, b_x:int=0, b_y:int=0):
 		self.text = text
@@ -13,14 +15,17 @@ class Button:
 		self.text_x = b_x
 		self.text_y = b_y
 
+
 	def update_coordinates(self):
 		self.image.set_current_rect()
+
 
 	def draw_button(self, window):
 		self.update_coordinates()
 		window.blit(self.image.image, (self.image.x, self.image.y))
 		self.draw_text(window)
 
+
 	def draw_text(self, window):
-		text = game_font.render(self.text, True, (2, 36, 8))
+		text = game_font.render(self.text, True, (Button.TEXT_COLOUR))
 		window.blit(text, (self.text_x,self.text_y))
