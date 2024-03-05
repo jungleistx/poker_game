22f15a887f46b69536588472dc056fd3960a1446
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-03-05 02:24:27 +0200

    mod Game, add set_bet to call player.bet()

--- classes/game.py
+++ classes/game.py
@@ -5,16 +5,24 @@ from classes.deck import Deck
 class Game:
 	nr_of_players = 0
 
+
 	def __init__(self):
 		self.card_swap_used = False
 		self.deck = Deck()
 		self.player = self.add_player()
+		self.bet = 0
+
 
 	def add_player(self) -> Player:
 		Game.nr_of_players += 1
 		return Player(self.deck)
 
+
 	def reset_deck(self):
 		self.deck.reset_deck()
 		self.player.reset_hand()
 		self.card_swap_used = False
+
+
+	def set_bet(self, bet):
+		self.bet = self.player.bet()
